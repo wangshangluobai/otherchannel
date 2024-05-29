e364a5aeac1a9ff2be545e13f6f0be938546c517
otherchannel <otherchannel@163.com>
2024-05-29 14:04:47 +0800

    1. 更新jest配置及添加相关注释

--- JavaScript/Library/jest.config.cjs
+++ JavaScript/Library/jest.config.cjs
@@ -1,7 +1,17 @@
 module.exports = {
+  // 是否在出现第一个错误时停止整个测试
+  bail: false,
+  // 运行中打印每个单独的测试
+  verbose: false,
+  // 匹配运行文件
+  testRegex: '\\.(test)\\.js$',
+  // 匹配路径忽略
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'],
+  // 测试环境 可选择 jsdom 或 node
   testEnvironment: 'node',
-  testRegex: '\\.(test|spec)\\.js$',
+  // 按顺序匹配被省略后缀名的文件类型
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx'],
+  // 通过babel进行预处理
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
   }
